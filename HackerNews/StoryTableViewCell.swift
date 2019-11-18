0381a21ee8431b8316514a91a95182b058ff4eb3
bssrdf <bssrdf@gmail.com>
2019-11-17 22:06:09 -0500

    added StoryTableViewCell; created its prototype in IB

---
+++ HackerNews/StoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StoryTableViewCell.swift
+//  HackerNews
+//
+//  Created by Shanshan Wang on 11/17/19.
+//  Copyright © 2019 Amit Burstein. All rights reserved.
+//
+
+import UIKit
+
+class StoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
