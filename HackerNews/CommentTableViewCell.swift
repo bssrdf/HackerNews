c8bd0a0c67ddb4ce4552f7aa2f40101ec204ce5d
bssrdf <bssrdf@gmail.com>
2019-11-29 19:38:46 -0500

    switched to a new method of getting dynamic cell height from articel 'self-sizing table view cells' which gives a more reliable height

--- HackerNews/CommentTableViewCell.swift
+++ HackerNews/CommentTableViewCell.swift
@@ -36,6 +36,9 @@ class CommentTableViewCell: UITableViewCell {
           let fullAttributed = NSMutableAttributedString(attributedString: usernameAttributed)
           fullAttributed.append(dateAttribute)
 
+          let indentation = CommentCellMarginConstant + (CommentCellMarginConstant * CGFloat(self.comment.level))
+          self.usernameLeftMarginContraint.constant = indentation
+          self.commentLeftMarginContraint.constant = indentation
           //self.commentTextView.font = UIFont.systemFont(ofSize: CommentCellFontSize)
           //self.commentTextView.textColor = UIColor.CommentLightGrayColor()
 
@@ -50,43 +53,44 @@ class CommentTableViewCell: UITableViewCell {
 
   
     
-    @IBOutlet weak var authorLabel: UILabel!
+    //@IBOutlet weak var authorLabel: UILabel!
     
+    @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var commentTextView: UITextView!
     
-    @IBOutlet weak var commentHeightConstrain: NSLayoutConstraint!
+    @IBOutlet weak var usernameLeftMarginContraint: NSLayoutConstraint!
+  
+    @IBOutlet weak var commentLeftMarginContraint: NSLayoutConstraint!
     
-    @IBOutlet weak var commentLeftMarginConstraint: NSLayoutConstraint!
-    
-    @IBOutlet weak var usernameLeftMarginConstrain: NSLayoutConstraint!
+  
   
     
   
-  var indentation: CGFloat {
+ /* var indentation: CGFloat {
       didSet {
           self.commentLeftMarginConstraint.constant = indentation
-          self.usernameLeftMarginConstrain.constant = indentation
-          self.commentHeightConstrain.constant =
+         // self.usernameLeftMarginConstrain.constant = indentation
+         /* self.commentHeightConstrain.constant =
               self.contentView.frame.size.height - CommentCellUsernameHeight - CommentCellTopMargin -
-                  CommentCellMarginConstant + 5.0
+                  CommentCellMarginConstant + 5.0*/
             
-          self.contentView.setNeedsUpdateConstraints()
+          //self.contentView.setNeedsUpdateConstraints()
       }
-  }
+  }*/
   
-  required init?(coder aDecoder: NSCoder) { // required for Xcode6-Beta5
-      self.indentation = CommentCellMarginConstant
+  /*required init?(coder aDecoder: NSCoder) { // required for Xcode6-Beta5
+     // self.indentation = CommentCellMarginConstant
       super.init(coder: aDecoder)
   }
 
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-      self.indentation = CommentCellMarginConstant
+    //  self.indentation = CommentCellMarginConstant
       super.init(style: style, reuseIdentifier: reuseIdentifier)
-  }
+  }*/
 
 
     
-    override func awakeFromNib() {
+    /*override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         //self.commentTextView.layer.borderColor = UIColor.black.cgColor
@@ -97,17 +101,17 @@ class CommentTableViewCell: UITableViewCell {
           25),
          NSAttributedString.Key.foregroundColor: UIColor.ReadingListColor()]
 
-    }
+    }*/
   
-   override func layoutSubviews() {
+  /* override func layoutSubviews() {
       super.layoutSubviews()
 
       //self.commentTextView.text = comment.text
     //print("coment id: \(comment.id) with text: \(String(describing: comment.text))")
 
-      self.commentTextView.textContainer.lineFragmentPadding = 0
+      /*self.commentTextView.textContainer.lineFragmentPadding = 0
       self.commentTextView.textContainerInset = UIEdgeInsets.zero
-      self.commentTextView.contentInset = UIEdgeInsets.zero
+      self.commentTextView.contentInset = UIEdgeInsets.zero*/
     
       
       //if comment.by == "williamdclt"{
@@ -116,25 +120,25 @@ class CommentTableViewCell: UITableViewCell {
         print("layoutSubviews : \(comment.text!.count) - \(comment.level) - \(x)")
         print("layoutSubviews : \(self.contentView.bounds.width) - \(self.commentLeftMarginConstraint.constant)")
       }*/
-      self.commentTextView.frame.size.width = self.contentView.bounds.width - (self.commentLeftMarginConstraint.constant * 1) -
-         (CommentCellMarginConstant * CGFloat(self.comment.level))
+      /*self.commentTextView.frame.size.width = self.contentView.bounds.width - (self.commentLeftMarginConstraint.constant * 1) -
+         (CommentCellMarginConstant * CGFloat(self.comment.level))*/
     
-      self.indentation = CommentCellMarginConstant + (CommentCellMarginConstant * CGFloat(self.comment.level))
+   //   self.indentation = CommentCellMarginConstant + (CommentCellMarginConstant * CGFloat(self.comment.level))
      /*if comment.by == user_debug{
        print("layoutSubviews const: \(self.commentHeightConstrain.constant)")
       print("layoutSubviews size.height : \(self.commentTextView.frame.size.height)")
       print("layoutSubviews author height: \(self.authorLabel.frame.size.height)")
      }*/
     
-  }
+  }*/
 
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
+    /*override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-  
+  */
   
    func URLAttributedText(input: String) -> NSMutableAttributedString {
     //let input = comment.text!
@@ -160,13 +164,7 @@ class CommentTableViewCell: UITableViewCell {
   }
   
   class func heightForText(text: String, bounds: CGRect, level: Int, author: String) -> CGFloat {
-      //if author == "williamdclt"{
-      /*if author == "colllectorof"{
-          let x = bounds.width - (CommentCellMarginConstant * 2) -
-               (CommentCellMarginConstant * CGFloat(level))
-           print("class height: \(text.count) - \(level) - \(x)")
-           print("class height: \(bounds.width)")
-        }*/
+      
           let size = text.boundingRect(with: CGSize(width: bounds.width - (CommentCellMarginConstant * 1) -
             
               (CommentCellMarginConstant * CGFloat(level)),
@@ -176,21 +174,9 @@ class CommentTableViewCell: UITableViewCell {
                   attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: CommentCellFontSize)
                        ],
               context: nil)
-      /* if author == user_debug {
-        
-        let x = CommentCellMarginConstant + CommentCellUsernameHeight + CommentCellTopMargin + ceil(size.height) + CommentCellBottomMargin
-        print("class height: \(size.height) - \(x)")
-        let y = bounds.width - (CommentCellMarginConstant * 1) -
-              (CommentCellMarginConstant * CGFloat(level))
-        print("class height: \(bounds.width) - \(y)")
-       }*/
+      
     return CommentCellMarginConstant + CommentCellUsernameHeight + CommentCellTopMargin + ceil(size.height) + CommentCellBottomMargin
   }
   
-  // Helper function inserted by Swift 4.2 migrator.
-  fileprivate func convertToOptionalNSAttributedStringKeyDictionary(_ input: [String: Any]?) -> [NSAttributedString.Key: Any]? {
-      guard let input = input else { return nil }
-      return Dictionary(uniqueKeysWithValues: input.map { key, value in (NSAttributedString.Key(rawValue: key), value)})
-  }
 
 }
--- HackerNews/DetailTableViewController.swift
+++ HackerNews/DetailTableViewController.swift
@@ -41,7 +41,8 @@ class DetailTableViewController: UITableViewController,
 
         
         retrieveComments()
-      
+      tableView.rowHeight = UITableView.automaticDimension
+      tableView.estimatedRowHeight = 600
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
@@ -50,14 +51,14 @@ class DetailTableViewController: UITableViewController,
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 2
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
       
-      if section == 0 {
+     /* if section == 0 {
         return 1
-      }
+      }*/
         // #warning Incomplete implementation, return the number of rows
       //if let comments = comments{
       //print("# of comments is \(comments.count)")
@@ -66,7 +67,7 @@ class DetailTableViewController: UITableViewController,
       //return 0
     }
   
-    override func tableView(_ tableView: UITableView,
+    /*override func tableView(_ tableView: UITableView,
              heightForRowAt indexPath: IndexPath) -> CGFloat {
       if (indexPath.section == 0) {
           let title: NSString = self.story!.title as NSString
@@ -76,7 +77,7 @@ class DetailTableViewController: UITableViewController,
       //print("\(comment.by): \(self.cellHeightCache[indexPath.row])")
       return self.cellHeightCache[indexPath.row] as CGFloat
      
-   }
+   }*/
    /*func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
       if (indexPath.section == 0) {
           let title: NSString = self.post.title! as NSString
@@ -87,7 +88,7 @@ class DetailTableViewController: UITableViewController,
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if (indexPath.section == 0) {
+       /* if (indexPath.section == 0) {
           let cellIdentifier = "StoryTableViewCell"
           guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as? StoryTableViewCell else{
             fatalError("The dequeued cell is not an instance of StoryTableViewCell")
@@ -95,7 +96,7 @@ class DetailTableViewController: UITableViewController,
             cell.post = self.story
             //cell!.cellDelegate = self;
             return cell
-        }
+        }*/
       
         let cellIdentifier = "CommentTableViewCell"
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? CommentTableViewCell else{
@@ -116,7 +117,7 @@ class DetailTableViewController: UITableViewController,
   
   // MARK: UITableViewDelegate
   
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+  /*override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     if (indexPath.section == 0) {
       tableView.deselectRow(at: indexPath, animated: true)
       
@@ -129,7 +130,7 @@ class DetailTableViewController: UITableViewController,
         present(webViewController, animated: true, completion: nil)
       }
     }
-  }
+  }*/
   
   // MARK: SFSafariViewControllerDelegate
   
